eb2fbd2b11f97e67ff8365d3e0282b5e53ac788d
george udosen <datameshprojects@gmail.com>
2018-03-02 04:36:20 +0100

    API call successful.

--- capstone/app/scripts/controllers/shopping.js
+++ capstone/app/scripts/controllers/shopping.js
@@ -8,10 +8,18 @@
  * Controller of the capstoneApp
  */
 angular.module('capstoneApp')
-  .controller('ShoppingCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ShoppingCtrl', ['$scope', 'productService', function ($scope, productService) {
+    $scope.data = {};
+    $scope.myProducts = function () {
+      // alert(JSON.stringify(productService.getProducts().data));
+      productService.getProducts()
+      .then(function (data) {
+        $scope.data = data;
+        alert(JSON.stringify($scope.data));
+
+      }, function (response) {
+          alert("There was an arror: " + response);
+      })
+    }
+
+  }]);
---
+++ capstone/app/scripts/service/productService.js
@@ -0,0 +1,32 @@
+/****************************************************
+ Product Service to get the product information from
+ the API service
+ ****************************************************/
+
+angular.module("capstoneApp").service("productService", function($http, $q) {
+  var baseUrl =
+      "https://webmppcapstone.blob.core.windows.net/data/itemsdata.json";
+  this.getProducts = function() {
+    var deferred = $q.defer();
+    $http({
+      method: "GET",
+      url: baseUrl,
+      dataType: 'json'
+    })
+      .then(function successCallback(data) {
+        deferred.resolve(data);
+      })
+      ,(function errorCallback(response) {
+        deferred.reject("There was an error: " + response);
+      });
+
+      return deferred.promise;
+    // $http.get("https://webmppcapstone.blob.core.windows.net/data/itemsdata.json")
+    //   .then(function(response) {
+    //     JSON.stringify(response)
+    //     // this.product = JSON.stringify(response);
+    //     console.log(JSON.stringify(response));
+    //     // alert(JSON.stringify(response));
+    //   })
+  };
+});
