8e1335103baef638b437600e3f68f8775c84e7de
george udosen <datameshprojects@gmail.com>
2018-03-16 18:13:19 +0100

    finished creating addedItemsList.

--- capstone/app/scripts/service/cartFactroyService.js
+++ capstone/app/scripts/service/cartFactroyService.js
@@ -6,9 +6,10 @@
 angular
   .module("capstoneApp")
   .service("cartFactoryService", ["$window", cartFactoryService]);
+
 function cartFactoryService($window) {
-    var addedItemsList = [];
-    var itemsFromStorage = [];
+  var addedItemsList = [];
+  var itemsFromStorage = [];
 
   function save(key, value) {
     $window.sessionStorage.setItem(key, JSON.stringify(value));
@@ -27,66 +28,47 @@ function cartFactoryService($window) {
   }
 
   function addItemToCart(nameOfItem, itemQuantity = 1) {
+    var result = false;
 
-    // Persist the items list between page refresh
-    if (!get('addedItemsList')) {
-      $window.alert("Creating new items list in session storage");
-      save("addedItemsList", addedItemsList);
-    } else {
-      $window.alert("Getting the items list from sessionStorage");
-      addedItemsList = get('addedItemsList');
-    }
+    // Create the addedItemsList both in the app
+    // and in the sessionStorage
+    if (get('addedItemsList')) {
+      addedItemsList = JSON.parse(get('addedItemsList'));
+      // check for duplicates and only update
+      // the quantity value
+      $window.alert("Check for duplicates");
 
-    $window.alert(get('addedItemsList'));
-
-    $window.alert("addedItemsList " + JSON.stringify(addedItemsList));
-
-    $window.alert("Inside addItemToCart function");
-
-    // Check to see if the addedItemsList Array is empty
-    if (addedItemsList.length !== 0) {
-      $window.alert("Inside if loop");
-      angular.forEach(addedItemsList, function(i) {
-        // Check if item exists
-        if (i.name === nameOfItem) {
-          $window.alert("Checking if ite already exists");
-          // If it does increase the quantity
-          i.quantity += quantity;
-        } else {
-          $window.alert("Add new items");
-          // Add new items to temp items list
-          this.addedItemsList.push({
-            'name': nameOfItem,
-            'quantity': itemQuantity
-          });
+      for (var i of addedItemsList) {
+        if (i['name'] === nameOfItem) {
+          i['quantity'] += itemQuantity;
+          result = true;
+          break;
         }
-      });
+      }
+
+      if (!result) {
+        $window.alert("Pusing into addedItemsList");
+        addedItemsList.push({
+          'name': nameOfItem,
+          'quantity': itemQuantity
+        });
+      }
+
+      save('addedItemsList', addedItemsList);
+      $window.alert("addedItemsList " + JSON.stringify(addedItemsList));
     } else {
-      $window.alert("Outside else loop, add new item");
-      // Add items to temp items list
       addedItemsList.push({
         'name': nameOfItem,
         'quantity': itemQuantity
       });
+      save('addedItemsList', addedItemsList);
+      $window.alert("addedItemsList " + JSON.stringify(addedItemsList));
+
     }
-    $window.alert("addedItemsList" + JSON.stringify(addedItemsList));
 
-    // Get the items collection from sessionStorage
-    var products = JSON.parse(get("shopItems"));
-    $window.alert("shopItems " + JSON.stringify(products));
-    //
-    angular.forEach(products, function(p) {
-      angular.forEach(addedItemsList, function(q) {
-        if (p.name === q.name) {
-          p.quantity = q.quantity;
-          itemsFromStorage.push(p);
-        }
-      });
-    });
 
-    $window.alert("itemsFromStorage " + JSON.stringify(itemsFromStorage));
-    // Create sessionStorage
-    save("cartItemsFromStorage", itemsFromStorage);
+
+
   }
 
   return {
