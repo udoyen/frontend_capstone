c7e32e3072f1489731b6475d3379713578bce04e
george udosen <datameshprojects@gmail.com>
2018-03-19 02:35:29 +0100

    done with items filtering

--- capstone/app/scripts/app.js
+++ capstone/app/scripts/app.js
@@ -53,6 +53,11 @@ angular
           controller: 'ProductCtrl',
           controllerAs: 'product'
         })
+        .when('/product/:category', {
+          templateUrl: 'views/product.html',
+          controller: 'ProductCtrl',
+          controllerAs: 'product'
+        })
         .when('/details/:itemName', {
           templateUrl: 'views/details.html',
           controller: 'DetailsCtrl',
--- capstone/app/scripts/controllers/product.js
+++ capstone/app/scripts/controllers/product.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * @ngdoc function
@@ -7,131 +7,133 @@
  * # ProductCtrl
  * Controller of the capstoneApp
  */
-angular.module('capstoneApp')
-  .controller('ProductCtrl', [
-    '$routeParams',
-    '$window',
-    '$scope',
-    'productService',
-    '$route',
-    'cartFactoryService',
-    function ($routeParams, $window, $scope, productService, $route, cartFactoryService) {
-      console.log('Subcategory' + $route.current.params.subcategory);
-      console.log('Category' + $routeParams.category);
-      $scope.subcategory = $routeParams.subcategory;
-      $scope.category = $routeParams.category;
-      $scope.singleCategory = {};
-      $scope.itemsNameArrayList = [];
-      $scope.itemsArrayList = [];
-      $scope.dropdownChoiceIndex;
-      $scope.subcatArrayListItems = [];
-      $scope.allProductsData = {};
-      $scope.filteredItemsArrayList = [];
-      $scope.sortFilterArray = [
-        'none', 'price', 'alphabetical', 'rating'
-      ];
-      $scope.categoriesList = {
-        'Household and Beauty': 0,
-        'Pantry Items': 1,
-        'Perishable': 2,
-        'Produce': 3
-      };
+// TODO: Clean up this fle
+angular.module("capstoneApp").controller("ProductCtrl", [
+  "$routeParams",
+  "$window",
+  "$scope",
+  "productService",
+  "$route",
+  "cartFactoryService",
+  "$location",
+  function(
+    $routeParams,
+    $window,
+    $scope,
+    productService,
+    $route,
+    cartFactoryService,
+    $location
+  ) {
+    console.log("Subcategory" + $route.current.params.subcategory);
+    console.log("Category" + $routeParams.category);
+    $scope.subcategory = $routeParams.subcategory;
+    $scope.category = $routeParams.category;
+    $scope.currentPath = $location.path();
+    $scope.singleCategory = {};
+    $scope.itemsNameArrayList = [];
+    $scope.itemsArrayList = [];
+    $scope.dropdownChoiceIndex;
+    $scope.subcatArrayListItems = [];
+    $scope.allProductsData = {};
+    $scope.filteredItemsArrayList = [];
+    $scope.sortFilterArray = ["none", "price", "alphabetical", "rating"];
+    $scope.categoriesList = {
+      "Household and Beauty": 0,
+      "Pantry Items": 1,
+      Perishable: 2,
+      Produce: 3
+    };
 
-      $scope.addItemToCartFromProductspage = cartFactoryService.addItemToCart;
-      $scope.allShopItems = [];
+    $scope.addItemToCartFromProductspage = cartFactoryService.addItemToCart;
+    $scope.allShopItems = [];
 
-      $scope.getDropdownIndex = function (dIndex) {
-        $scope.dropdownChoiceIndex = $scope.sortFilterArray[dIndex];
-      }
+    $scope.getDropdownIndex = function(dIndex) {
+      $scope.dropdownChoiceIndex = $scope.sortFilterArray[dIndex];
+    };
 
+    $scope.subcategoriesProductList = function() {
+      productService.getProducts().then(function(products) {
+        $scope.allProductsData = products;
+        console.log(products);
+      });
 
-
-      $scope.subcategoriesProductList = function () {
-        productService
-        .getProducts()
-        .then(function (products) {
-          $scope.allProductsData = products;
-          console.log(products);
-        });
-
-        /**
-         * Get data from productService
-         */
-        productService
+      /**
+       * Get data from productService
+       */
+      productService
         .getProducts()
         .then(function(products) {
-              $scope.subcategory = $routeParams.subcategory;
-              $scope.category = $routeParams.category;
-              console.log(typeof $scope.subcategory);
-              console.log(typeof $scope.category);
+          $scope.subcategory = $routeParams.subcategory;
+          $scope.category = $routeParams.category;
+          console.log(typeof $scope.subcategory);
+          console.log(typeof $scope.category);
+          $window.alert($scope.currentPath);
 
-            // Get the index of the categoryName
-            angular.forEach($scope.categoriesList, function(value, key) {
-              if (key === $scope.category) {
-                $scope.singleCategory = products[value].subcategories;
-                console.log($scope.singleCategory);
-                $scope.category = products[value].category;
-                console.log($scope.category);
+          // Get the index of the categoryName
+          angular.forEach($scope.categoriesList, function(value, key) {
+            if (key === $scope.category) {
+              $scope.singleCategory = products[value].subcategories;
+              console.log($scope.singleCategory);
+              $scope.category = products[value].category;
+              console.log($scope.category);
+            }
+          });
 
-              }
+          console.log(JSON.stringify($scope.singleCategory));
+
+          // Create ArrayList of subcategory items
+          angular.forEach($scope.singleCategory, function(key, value) {
+            angular.forEach(key.items, function(key, value) {
+              $scope.itemsNameArrayList.push(key.name);
+              $scope.itemsArrayList.push(key);
+              $scope.subcatArrayListItems.push(key);
             });
+          });
+          // Create filtered items
+          // angular.forEach($scope.subcatArrayListItems, function (key, value) {
+          //   if (angular.equals(key.subcategory, $scope.subcategory)) {
+          //     console.log(JSON.stringify(key));
+          //     $scope.filteredItemsArrayList.push(key);
+          //   }
 
-            console.log(JSON.stringify($scope.singleCategory));
+          // })
 
-            // Create ArrayList of subcategory items
-            angular.forEach($scope.singleCategory, function (key, value) {
-              angular.forEach(key.items, function (key, value) {
-                $scope.itemsNameArrayList.push(key.name);
-                $scope.itemsArrayList.push(key);
-                $scope.subcatArrayListItems.push(key);
+          angular.forEach(products, function(key, value) {
+            angular.forEach(key.subcategories, function(key, value) {
+              angular.forEach(key.items, function(key, value) {
+                $scope.allShopItems.push(key);
+              });
+            });
+          });
 
-              })
+          // The items in session storage
+          cartFactoryService.save("shopItems", $scope.allShopItems);
 
-            })
-            // Create filtered items
-            // angular.forEach($scope.subcatArrayListItems, function (key, value) {
-            //   if (angular.equals(key.subcategory, $scope.subcategory)) {
-            //     console.log(JSON.stringify(key));
-            //     $scope.filteredItemsArrayList.push(key);
-            //   }
+          console.log($scope.allShopItems);
 
-            // })
+          // for (let index = 0; index <  $scope.itemsNameArrayList.length; index++) {
+          //   console.log( $scope.itemsNameArrayList[index]);
 
-
-              angular.forEach(products, function (key, value) {
-                angular.forEach(key.subcategories, function (key, value) {
-                  angular.forEach(key.items, function (key, value) {
-                    $scope.allShopItems.push(key);
-                  })
-                })
-              })
-
-              // The items in session storage
-              cartFactoryService.save('shopItems', $scope.allShopItems);
-
-              console.log($scope.allShopItems);
-
-
-            // for (let index = 0; index <  $scope.itemsNameArrayList.length; index++) {
-            //   console.log( $scope.itemsNameArrayList[index]);
-
-            // }
-            // console.log($scope.itemsNameArrayList.toString());
-            console.log('List items' + JSON.stringify($scope.subcatArrayListItems));
-            console.log('Items Array List' + JSON.stringify($scope.itemsArrayList));
-
+          // }
+          // console.log($scope.itemsNameArrayList.toString());
+          console.log(
+            "List items" + JSON.stringify($scope.subcatArrayListItems)
+          );
+          console.log(
+            "Items Array List" + JSON.stringify($scope.itemsArrayList)
+          );
         })
         .catch(function(e) {
-          console.log('There was an error: ' + e);
+          console.log("There was an error: " + e);
         });
-      };
+    };
 
-      // Use this function to load the api result into the page
-      $scope.init = function() {
-      console.log('Angular call function on product page load');
+    // Use this function to load the api result into the page
+    $scope.init = function() {
+      console.log("Angular call function on product page load");
       $scope.subcategoriesProductList();
       // getAllShopItems();
     };
-
   }
-  ]);
+]);
